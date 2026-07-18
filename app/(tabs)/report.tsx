13fa7e8f93997af767085d5455ca9badd7745271
santy <cativasantiago1@gmail.com>
2026-07-18 13:58:20 -0300

    ADD: logic for redirect user no login

--- app/(tabs)/report.tsx
+++ app/(tabs)/report.tsx
@@ -1,8 +1,32 @@
 import { ReportForm } from "@/src/components/report/ReportForm";
+import { useAuth } from "@/src/context/AuthContext";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 import { useLocalSearchParams } from "expo-router";
+import { themeColors } from "@/src/theme/theme";
+import { useEffect } from "react";
+import { router } from "expo-router";
+
 
 export default function ReportScreen() {
   const params = useLocalSearchParams<{ reportSpId?: string }>();
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator
+          size="large"
+          color={themeColors.primary}
+        />
+      </View>
+    );
+  }
+
+  useEffect(() => {
+    if (!loading && !user) {
+      router.push("/login");
+    }
+  }, [loading, user]);
 
   return (
     <ReportForm
@@ -11,8 +35,20 @@ export default function ReportScreen() {
         console.log("Reporte enviado con exito");
       }}
       onSubmitError={(message) => {
-        console.error("Error al enviar el reporte", message);
+        console.error(
+          "Error al enviar el reporte",
+          message
+        );
       }}
     />
   );
 }
+
+const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: themeColors.screenBackground,
+  },
+});
